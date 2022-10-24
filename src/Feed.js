a28ac274d39c8abbc558f0af9aa325978846c0b6
farmology <89947667+farmology@users.noreply.github.com>
2022-10-24 13:25:17 -0700

    Create input options for top of feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
   return (
@@ -10,9 +15,15 @@ function Feed() {
                 <CreateIcon />
                 <form>
                     <input type='text'/>
-                    <button type='submitt'>Send</button>
+                    <button type='submit'>Send</button>
                 </form>
             </div>
+          <div className="feed__inputOptions">
+            <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
+            <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E'/>
+            <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD'/>
+            <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
+          </div>
         </div>
     </div>
   )
