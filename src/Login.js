42bd0efed1c58630f01931b3bc91024dfe342f32
farmology <89947667+farmology@users.noreply.github.com>
2022-10-25 00:44:06 -0700

    Create user in Redux & login page

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className='login'>You are not logged in. Access denied.</div>
+  )
+}
+
+export default Login
