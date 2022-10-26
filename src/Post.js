4214809c9668f654cc401ba4289e1da5cc02c1de
farmology <89947667+farmology@users.noreply.github.com>
2022-10-26 02:10:59 -0700

    Make posts pull name & pic from user info

--- src/Post.js
+++ src/Post.js
@@ -7,11 +7,11 @@ import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 
-function Post({ name, description, message, }) {
+function Post({ name, description, message, photoUrl }) {
   return (
     <div className='post'>
         <div className="post__header">
-            <Avatar />
+            <Avatar src={photoUrl}>{name[0]}</Avatar>
             <div className="post__info">
                 <h2>{name}</h2>
                 <p>{description}</p>
