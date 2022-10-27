32e29eac9cb869a153e1b1f9c74b5c1e951052a0
farmology <89947667+farmology@users.noreply.github.com>
2022-10-26 21:13:44 -0700

    Add and style widgets section

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import InfoIcon from '@mui/icons-material/Info';
 import './Widgets.css';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 function Widgets() {
   
     const newsArticle = (heading, subtitle) => (
         <div className='widgets__article'>
-            <div className='widgets__articleLeft'></div> 
-            <div className='widgets__articleRight'></div> 
+            <div className='widgets__articleLeft'>
+                <FiberManualRecordIcon />
+            </div> 
+            <div className='widgets__articleRight'>
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div> 
         </div>
     )
     return (
@@ -16,6 +22,8 @@ function Widgets() {
             <h2>LinkedIn News</h2>
             <InfoIcon />
         </div>
+        {newsArticle('Microsoft buys Blizzard', 'Tech giant acquires game company in a landmark deal')}
+        {newsArticle('Elon Musk spotted wearing Crocs', 'Millions of aspiring tech bros sell out shoe stores to emulate their esteemed idol')}
     </div>
   )
 }
